bac5842038d0eae6440440f4a3f555f7c690eac5
Josenilton Fernandes <josenilton.fernandes@original.com.br>
2020-08-29 17:56:35 -0300

    adicao da solução do teste Java End-of-file

---
+++ src/Java End-of-file/JavaEOF.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+// Java End-of-file
+public class JavaEOF {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int line_num = 0;
+        while(sc.hasNext()){
+            System.out.println(++line_num + " " + sc.nextLine());
+        }
+        sc.close();
+    }
+}
