5434786f620f1429139661899c4923880f3bf017
Josenilton Fernandes <josenilton.fernandes@original.com.br>
2020-08-31 14:01:52 -0300

    adicao da solução do teste Java String Reverse

---
+++ src/Java String Reverse/JavaStringReverse.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class JavaStringReverse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        sc.close();
+        StringBuilder stringReverse = new StringBuilder();
+        stringReverse.append(A); 
+        stringReverse = stringReverse.reverse(); 
+        System.out.println(A.equals(stringReverse.toString()) == true);
+    }
+}
